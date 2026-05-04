b8b05591e3cd490d4659e683b5ca0e524d08a002
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-04 19:28:14 +0300

    добавил конфиг

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServerPort string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+}
+
+func load() (*Config, error) {
+	_ = godotenv.Load()
+
+	cfg := &Config{
+		ServerPort: getEnv("SERVER_PORT"),
+		DBHost:     getEnv("DB_HOST"),
+		DBPort:     getEnv("DB_PORT"),
+		DBUser:     getEnv("DB_USER"),
+		DBPassword: getEnv("DB_PASSWORD"),
+		DBName:     getEnv("DB_NAME"),
+		DBSSLMode:  getEnv("DB_SSLMODE"),
+	}
+
+	return cfg, nil
+}
+
+func getEnv(key string) string {
+	value := os.Getenv(key)
+	return value
+}

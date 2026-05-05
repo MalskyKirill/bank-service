a315a63eda0e2a06e41482c18dfecfb3e6806562
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-05 19:30:16 +0300

    добавил health-handler

---
+++ handler/healf-heandler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type HealthResponse struct {
+	Status    string    `json:"status"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	response := HealthResponse{
+		Status:    "ok",
+		Timestamp: time.Now(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_ = json.NewEncoder(w).Encode(response)
+}

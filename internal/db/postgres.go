71b6b225c41f78d08955227d2f01400f3bd143b6
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-06 10:45:47 +0300

    добавил docker-compose для бд

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"fmt"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 func OpenPostgres(cfg *config.Config) (*sql.DB, error) {
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import (
+	"bank-service/internal/handler"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewRouter() http.Handler {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/health", handler.Health).Methods(http.MethodGet)
+	return r
+}

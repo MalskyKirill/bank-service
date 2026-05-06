970d222b3d374e55c66946a83d97ed77f23e7955
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-06 16:20:18 +0300

    добавил репозиторий пользователя

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,103 @@
+package repository
+
+import (
+	"bank-service/internal/models"
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) Create(ctx context.Context, user *models.User) error {
+	query := `
+		INSERT INTO users (email, username, password_hash)
+		VALUES ($1, $2, $3)
+		RETURNING id, created_at
+		`
+	err := r.db.QueryRowContext(
+		ctx,
+		query,
+		user.Email,
+		user.Username,
+		user.PasswordHash,
+	).Scan(&user.ID, &user.CreatedAt)
+
+	if err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return nil
+}
+
+func (r *UserRepository) FindByEmail(ctx context.Context, email string) (*models.User, error) {
+	query := `
+		SELECT id, email, username, password_hash, created_at
+		FROM users
+		WHERE id = $1
+	`
+
+	var user models.User
+
+	err := r.db.QueryRowContext(
+		ctx,
+		query,
+		email,
+	).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Username,
+		&user.PasswordHash,
+		&user.CreatedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to find user by email: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (r *UserRepository) FindByUsername(ctx context.Context, username string) (*models.User, error) {
+	query := `
+		SELECT id, email, username, password_hash, created_at
+		FROM users
+		WHERE username = $1
+	`
+
+	var user models.User
+
+	err := r.db.QueryRowContext(
+		ctx,
+		query,
+		username,
+	).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Username,
+		&user.PasswordHash,
+		&user.CreatedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to find user by username: %w", err)
+	}
+
+	return &user, nil
+}

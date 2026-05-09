52ad3642137d14fae44468c205ffe98ecca675ff
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-09 12:02:53 +0300

    обновил роутер

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -14,10 +16,18 @@ type Config struct {
 	DBPassword string
 	DBName     string
 	DBSSLMode  string
+
+	JWTSecret   string
+	JWTTTLHours int
 }
 
 func Load() (*Config, error) {
 	_ = godotenv.Load()
+
+	jwtTTLHours, err := getEnvAsInt("JWT_TTL_HOURS")
+	if err != nil {
+		return nil, err
+	}
 
 	cfg := &Config{
 		ServerPort: getEnv("SERVER_PORT"),
@@ -27,6 +37,9 @@ func Load() (*Config, error) {
 		DBPassword: getEnv("DB_PASSWORD"),
 		DBName:     getEnv("DB_NAME"),
 		DBSSLMode:  getEnv("DB_SSLMODE"),
+
+		JWTSecret:   getEnv("JWT_SECRET"),
+		JWTTTLHours: jwtTTLHours,
 	}
 
 	return cfg, nil
@@ -35,4 +48,16 @@ func Load() (*Config, error) {
 func getEnv(key string) string {
 	value := os.Getenv(key)
 	return value
+}
+
+func getEnvAsInt(key string) (int, error) {
+	value := os.Getenv(key)
+
+	intValue, err := strconv.Atoi(value)
+
+	if err != nil {
+		return 0, fmt.Errorf("invalid value for %s: %w", key, err)
+	}
+
+	return intValue, nil
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,15 +1,30 @@
 package router
 
 import (
+	"bank-service/internal/config"
 	"bank-service/internal/handler"
+	"bank-service/internal/repository"
+	"bank-service/internal/security"
+	"bank-service/internal/service"
+	"database/sql"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
-func NewRouter() http.Handler {
+func NewRouter(database *sql.DB, cfg *config.Config, logger *logrus.Logger) http.Handler {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/health", handler.Health).Methods(http.MethodGet)
+
+	userRepository := repository.NewUserRepository(database)
+	jwtService := security.NewJWTService(cfg.JWTSecret, cfg.JWTTTLHours)
+	authService := service.NewAuthService(userRepository, jwtService)
+	authHandler := handler.NewAuthHandler(authService, logger)
+
+	r.HandleFunc("/register", authHandler.Register).Methods(http.MethodPost)
+	r.HandleFunc("/login", authHandler.Login).Methods(http.MethodPost)
+
 	return r
 }

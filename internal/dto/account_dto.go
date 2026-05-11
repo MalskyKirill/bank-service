62ab3126d1d99dde3d14952a853c8c6bc26a9094
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-11 12:00:19 +0300

    обновил account_dto добавил дтошки для операций со счетом

--- internal/dto/account_dto.go
+++ internal/dto/account_dto.go
@@ -13,3 +13,25 @@ type AccountResponse struct {
 	Currency  string    `json:"currency"`
 	CreatedAt time.Time `json:"created_at"`
 }
+
+type MoneyOperationRequest struct {
+	Amount int64 `json:"amount"`
+}
+
+type AccountOperationResponse struct {
+	Message string          `json:"message"`
+	Account AccountResponse `json:"account"`
+}
+
+type TransferRequest struct {
+	FromAccauntID int64   `json:"from_account_id"`
+	ToAccauntID   int64   `json:"to_account_id"`
+	Amount        float64 `json:"amount"`
+}
+
+type TransferResponce struct {
+	Message        string  `json:"message"`
+	FromAccauntID  int64   `json:"from_account_id"`
+	ToAccauntID    int64   `json:"to_account_id"`
+	TransferAmount float64 `json:"transfer_amount"`
+}

c9acd690e2d6ffeef677b2de36078472a8ba6b38
Kirill Malskiy <malskykirill@yandex.ru>
2026-05-15 14:55:49 +0300

    добавил модель кредит и платижей, добавил dto

---
+++ internal/dto/credit_dto.go
@@ -0,0 +1,38 @@
+package dto
+
+import "time"
+
+type CreateCreditRequest struct {
+	AccountId  int64   `json:"account_id"`
+	Amount     float64 `json:"amount"`
+	TermMonths int     `json:"term_months"`
+}
+
+type CreditResponse struct {
+	ID             int64     `json:"id"`
+	UserID         int64     `json:"user_id"`
+	AccountID      int64     `json:"account_id"`
+	Amount         float64   `json:"amount"`
+	InterestRate   float64   `json:"interest_rate"`
+	TermMonths     int       `json:"term_months"`
+	MonthlyPayment float64   `json:"monthly_payment"`
+	Status         string    `json:"status"`
+	CreatedAt      time.Time `json:"created_at"`
+}
+
+type PaymentSheduleResponse struct {
+	ID              int64      `json:"id"`
+	CreditID        int64      `json:"credit_id"`
+	PaymentDate     time.Time  `json:"payment_date"`
+	Amount          float64    `json:"amount"`
+	PrincipalAmount float64    `json:"principal_amount"`
+	InterestAmount  float64    `json:"interest_amount"`
+	PenaltyAmount   float64    `json:"penalty_amount"`
+	Status          string     `json:"status"`
+	PaidAt          *time.Time `json:"paid_at,omitempty"`
+}
+
+type CreateCreditResponse struct {
+	Credit  CreditResponse           `json:"credit"`
+	Shedule []PaymentSheduleResponse `json:"shedule"`
+}
---
+++ internal/models/credit.go
@@ -0,0 +1,27 @@
+package models
+
+import "time"
+
+type Credit struct {
+	ID             int64
+	UserID         int64
+	AccountID      int64
+	Amount         float64
+	InterestRate   float64
+	TermMonths     int
+	MonthlyPayment float64
+	Status         string
+	CreatedAt      time.Time
+}
+
+type PaymentSchedule struct {
+	ID              int64
+	CredinID        int64
+	PaymentDate     time.Time
+	Amount          float64
+	PrincipalAmount float64
+	InterestAmount  float64
+	PenaltyAmount   float64
+	Status          string
+	PaidAt          *time.Time
+}
